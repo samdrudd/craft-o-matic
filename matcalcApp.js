701ad704dc7812ac84414743a7a7d1b9af5bb394
Sam Rudd <samdrudd@gmail.com>
2018-05-30 01:05:06 -0700

    switch to XIVDB API, display first level of ingredients

--- matcalcApp.js
+++ matcalcApp.js
@@ -3,34 +3,23 @@ var app = angular.module('matcalc', []);
 app.factory('Recipe', ['$http', function($http) {
 	
 	var Recipe = {};
-	var urlBase = "http://127.0.0.1:8000/";
+	var urlBase = "http://api.xivdb.com/";
 		
 	Recipe.getAll = function() {
 		return $http({
-				url: urlBase + "recipes",
+				url: urlBase + "search",
+				params: {
+					"one" : "recipes",
+					"level|gt" : 325
+				},
 				method: "GET"});
 	};
 	
-	Recipe.add = function(recipe) {
+	Recipe.get = function(id) {
 		return $http({
-				url: urlBase + "recipes",
-				method: "POST",
-				data: $.param(recipe),
+				url: urlBase + "recipe/" + id,
+				method: "GET",
 				headers: { "Content-Type": "application/x-www-form-urlencoded"}});
-	};
-	
-	Recipe.edit = function(id, recipe) {
-		return $http({
-				url: urlBase + "recipes/" + id,
-				method: "PUT",
-				data: $.param(job),
-				headers: { "Content-Type": "application/x-www-form-urlencoded"}});
-	};
-	
-	Recipe.delete = function(id) {
-		return $http({
-				url: urlBase + "recipes/" + id,
-				method: "DELETE" });
 	};
 	
 	return Recipe;
--- matcalcCtrl.js
+++ matcalcCtrl.js
@@ -1,15 +1,39 @@
 app.controller("matcalcCtrl", ['$scope', '$timeout', '$filter', 'Recipe',
 	function($scope, $timeout, $filter, Recipe) {
 		$scope.recipes = [];
+		$scope.recipeSelection = {};
 		$scope.recipe = {};
 		
 		
 		$scope.init = function() {
 			Recipe.getAll()
 				.then(
-					(res) => { $scope.recipes = res.data; },
+					(res) => { $scope.recipes = res.data.recipes.results; },
 					(res) => {}
-				)
+				);
 		};
+				
+		$scope.getRecipe = function(recipe, callback) {
+			if (!recipe.connect_craftable) return;
+			
+			for (key in recipe.synths) {
+				console.log(key);
+				Recipe.get(key)
+					.then(
+						(res) => { console.log(res.data); },
+						(res) => {}
+					);
+			}
+		};
+				
+		$scope.$watch('recipeSelection', (newVal, oldVal, scope) => {
+			if (oldVal === newVal) return;
+			
+			Recipe.get(newVal.id)
+				.then(
+					(res) => { scope.recipe = res.data; },
+					(res) => {}
+				);
+		});
 	}
 ]);
